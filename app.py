84cd705d44e7b304929cc1bcc93eef71f78a655c
Matheus Muniz <108503370+Math-Muniz@users.noreply.github.com>
2025-10-31 23:41:37 -0300

    Refactor: Progress Bar in UI

--- app.py
+++ app.py
@@ -567,13 +567,13 @@ with st.sidebar:
     
     st.header("Status da Simulação")
     if st.session_state.current_session_num <= 7:
-        progress = (st.session_state.current_session_num - 1) / 7
-        st.progress(progress)
         st.info(
             f"Sessão: **{st.session_state.current_session_num}/7** | "
             f"Paciente: **{st.session_state.current_patient['name']}**", 
             icon="⚠️"
         )
+        progress = (st.session_state.current_session_num - 1) / 7
+        st.progress(progress)
     else:
         st.success("✅ Todas as 7 sessões concluídas!", icon="🎉")
     
